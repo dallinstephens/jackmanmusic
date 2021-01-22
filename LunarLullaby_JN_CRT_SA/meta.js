fbf44dca472cc204e28bb7caea99a85611e20602
dallinstephens <dallins@juno.com>
2021-01-22 10:05:32 -0700

    Add files via upload

---
+++ LunarLullaby_JN_CRT_SA/meta.js
@@ -0,0 +1,31 @@
+{
+  "song": {
+    "title": "Lunar Lullaby - SA",
+    "description": "Choir",
+    "composer": "By Jacob Narverud",
+    "albumArt": "LUNAR LULLABY.jpg",
+    "midiFile": "Lunar Lullaby SSAA.mid",
+    "track": [
+      {
+        "title": "Soprano 1",
+        "filename": "Lunar Lullaby SSAA Soprano 1.mp3"
+      },
+      {
+        "title": "Soprano 2",
+        "filename": "Lunar Lullaby SSAA Soprano 2.mp3"
+      },
+      {
+        "title": "Alto 1",
+        "filename": "Lunar Lullaby SSAA Alto 1.mp3"
+      },
+      {
+        "title": "Alto 2",
+        "filename": "Lunar Lullaby SSAA Alto 2.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "Lunar Lullaby SSAA Piano.mp3"
+      }
+    ]
+  }
+}
