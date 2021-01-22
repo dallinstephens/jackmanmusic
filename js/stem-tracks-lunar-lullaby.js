afd3acbe808b56a84b486d22cabc53412001afac
dallinstephens <dallins@juno.com>
2021-01-22 10:13:52 -0700

    Update stem-tracks-lunar-lullaby.js

--- js/stem-tracks-lunar-lullaby.js
+++ js/stem-tracks-lunar-lullaby.js
@@ -16,29 +16,29 @@ var playlist = WaveformPlaylist.init({
 
 playlist.load([
   {
-    "src": "LunarLullaby_JN_CRT_SA",
+    "src": "LunarLullaby_JN_CRT_SA/Lunar%20Lullaby%20SSAA%20Soprano%201.mp3",
     "name": "SSAA Soprano 1",
     "gain": 0.75,
     "muted": false,
     "soloed": false
   },
   {
-    "src": "LunarLullaby_JN_CRT_SA",
+    "src": "LunarLullaby_JN_CRT_SA/Lunar%20Lullaby%20SSAA%20Soprano%202.mp3",
     "name": "SSAA Soprano 2",
     "gain": 0.75
   },
   {
-    "src": "LunarLullaby_JN_CRT_SA",
+    "src": "LunarLullaby_JN_CRT_SA/Lunar%20Lullaby%20SSAA%20Alto%201.mp3",
     "name": "Alto 1",
     "gain": 0.75
   },
   {
-    "src": "LunarLullaby_JN_CRT_SA",
+    "src": "LunarLullaby_JN_CRT_SA/Lunar%20Lullaby%20SSAA%20Alto%202.mp3",
     "name": "Alto 2",
     "gain": 0.75
   },
   {
-    "src": "LunarLullaby_JN_CRT_SA",
+    "src": "LunarLullaby_JN_CRT_SA/Lunar%20Lullaby%20SSAA%20Piano.mp3",
     "name": "Piano",
     "gain": 0.75
   }
