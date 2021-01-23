1fa18a012efc97383b74abbb2c50501da4aa955b
dallinstephens <dallins@juno.com>
2021-01-22 18:14:27 -0700

    Update stem-tracks-homeward-bound-tbb.js

--- js/stem-tracks-homeward-bound-tbb.js
+++ js/stem-tracks-homeward-bound-tbb.js
@@ -1,1 +1,42 @@
+var playlist = WaveformPlaylist.init({
+  samplesPerPixel: 1000,
+  waveHeight: 100,
+  container: document.getElementById("playlist"),
+  timescale: true,
+  state: 'cursor',
+  colors: {
+    waveOutlineColor: '#E0EFF1'
+  },
+  controls: {
+    show: true, //whether or not to include the track controls
+    width: 200 //width of controls in pixels
+  },
+  zoomLevels: [500, 1000, 3000, 5000, 500]
+});
 
+playlist.load([
+  {
+    "src": "HomewardBound_JA_CRT_TBB/Homeward%20Bound%20TBB%20Tenor.mp3",
+    "name": "Tenor",
+    "gain": 0.75,
+    "muted": false,
+    "soloed": false
+  },
+  {
+    "src": "HomewardBound_JA_CRT_TBB/Homeward%20Bound%20TBB%20Baritone.mp3",
+    "name": "Baritone",
+    "gain": 0.75
+  },
+  {
+    "src": "HomewardBound_JA_CRT_TBB/Homeward%20Bound%20TBB%20Bass%202.mp3",
+    "name": "Bass",
+    "gain": 0.75
+  },
+  {
+    "src": "HomewardBound_JA_CRT_TBB/Homeward%20Bound%20TBB%20Piano.mp3",
+    "name": "Piano",
+    "gain": 0.75
+  }
+]).then(function() {
+  //can do stuff with the playlist.
+});
