718eb64c8724f899838ad9b8547df3fb49e11174
dallinstephens <dallins@juno.com>
2021-01-22 17:26:16 -0700

    Add files via upload

---
+++ FlightSong_KAA_CRT_SATB/meta.js
@@ -0,0 +1,39 @@
+{
+  "song": {
+    "title": "Flight Song - SATB",
+    "description": "Choir",
+    "composer": "By Kim Andre Arnesen",
+    "albumArt": "FLIGHT-SONG.jpg",
+    "midiFile": "Flight Song SATB.mid",
+    "track": [
+      {
+        "title": "Soprano",
+        "filename": "Flight Song SATB Soprano.mp3"
+      },
+      {
+        "title": "Alto 1",
+        "filename": "Flight Song SATB Alto 1.mp3"
+      },
+      {
+        "title": "Alto 2",
+        "filename": "Flight Song SATB Alto 2.mp3"
+      },
+      {
+        "title": "Tenor",
+        "filename": "Flight Song SATB Tenor.mp3"
+      },
+      {
+        "title": "Baritone",
+        "filename": "Flight Song SATB Baritone.mp3"
+      },
+      {
+        "title": "Bass 2",
+        "filename": "Flight Song SATB Bass 2.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "Flight Song SATB Piano.mp3"
+      }
+    ]
+  }
+}
