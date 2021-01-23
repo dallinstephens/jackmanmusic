b722f3d1e7013e88e17c19fc5591a49459e96a91
dallinstephens <dallins@juno.com>
2021-01-22 17:31:56 -0700

    Add files via upload

---
+++ ThreeQuotes_AR_CRT_SA/meta.js
@@ -0,0 +1,23 @@
+{
+  "song": {
+    "title": "Three Quotes by Mark Twain - SA",
+    "description": "Choir",
+    "composer": "By Andrea Ramsey",
+    "albumArt": "THREE-QUOTES-BY-MARK-TWAIN.jpg",
+    "midiFile": "Three Quotes SA.mid",
+    "track": [
+      {
+        "title": "Soprano",
+        "filename": "Three Quotes SA Soprano.mp3"
+      },
+      {
+        "title": "Alto",
+        "filename": "Three Quotes SA Alto.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "Three Quotes SA Piano.mp3"
+      }
+    ]
+  }
+}
