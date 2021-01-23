6bfb4fac39c000e36cfa0e1a674a209f13868b1d
dallinstephens <dallins@juno.com>
2021-01-22 17:15:49 -0700

    Add files via upload

---
+++ SesereEeye_MO_CRT_SSA/meta.js
@@ -0,0 +1,23 @@
+{
+  "song": {
+    "title": "Sesere Eeye - SSA",
+    "description": "Choir",
+    "composer": "Arr. by Mark O'Leary",
+    "albumArt": "SESERE-EEYE.jpg",
+    "midiFile": "Sesere Eeye.mid",
+    "track": [
+      {
+        "title": "Melody",
+        "filename": "Sesere Eeye Melody.mp3"
+      },
+      {
+        "title": "Harmony 1",
+        "filename": "Sesere Eeye Harmony 1.mp3"
+      },
+      {
+        "title": "Harmony 2",
+        "filename": "Sesere Eeye Harmony 2.mp3"
+      }
+    ]
+  }
+}
