ace66f48afb4dfc8c65dd291a6a84becd553f978
dallinstephens <dallins@juno.com>
2021-01-22 17:20:50 -0700

    Add files via upload

---
+++ OneSong_MH_CRT_SATB/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "One Song - SATB",
+    "description": "Choir",
+    "composer": "Arr. by Mark Hayes",
+    "albumArt": "ONE-SONG.jpg",
+    "midiFile": "One Song SATB.mid",
+    "track": [
+      {
+        "title": "Soprano",
+        "filename": "One Song SATB Soprano.mp3"
+      },
+      {
+        "title": "Alto",
+        "filename": "One Song SATB Alto.mp3"
+      },
+      {
+        "title": "Tenor",
+        "filename": "One Song SATB Tenor.mp3"
+      },
+      {
+        "title": "Bass",
+        "filename": "One Song SATB Bass.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "One Song SATB Piano.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "One Song SATB Solo.mp3"
+      }
+    ]
+  }
+}
