6910f9ee34187b12a5c12e730703dbda49dd48ff
dallinstephens <dallins@juno.com>
2021-01-22 17:22:55 -0700

    Add files via upload

---
+++ HomewardBound_JA_CRT_TBB/meta.js
@@ -0,0 +1,27 @@
+{
+  "song": {
+    "title": "Homeward Bound - TBB",
+    "description": "Choir",
+    "composer": "Arr. by Jay Althouse",
+    "albumArt": "Homeward-Bound.jpg",
+    "midiFile": "Homeward Bound TBB.mid",
+    "track": [
+      {
+        "title": "Tenor",
+        "filename": "Homeward Bound TBB Tenor.mp3"
+      },
+      {
+        "title": "Baritone",
+        "filename": "Homeward Bound TBB Baritone.mp3"
+      },
+      {
+        "title": "Bass 2",
+        "filename": "Homeward Bound TBB Bass 2.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "Homeward Bound TBB Piano.mp3"
+      }
+    ]
+  }
+}
