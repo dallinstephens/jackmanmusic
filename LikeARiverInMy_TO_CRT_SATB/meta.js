8efe939cc2b396d74bf055ffa4e919940ff97626
dallinstephens <dallins@juno.com>
2021-01-22 17:28:43 -0700

    Add files via upload

---
+++ LikeARiverInMy_TO_CRT_SATB/meta.js
@@ -0,0 +1,31 @@
+{
+  "song": {
+    "title": "Like a River In My Soul",
+    "description": "Choir",
+    "composer": "By Tim Osiek",
+    "albumArt": "LIKE-A-RIVER-IN-MY-SOUL.jpg",
+    "midiFile": "Like A River SATB.mid",
+    "track": [
+      {
+        "title": "Soprano",
+        "filename": "Like A River SATB Soprano.mp3"
+      },
+      {
+        "title": "Alto",
+        "filename": "Like A River SATB Alto.mp3"
+      },
+      {
+        "title": "Tenor",
+        "filename": "Like A River SATB Tenor.mp3"
+      },
+      {
+        "title": "Bass",
+        "filename": "Like A River SATB Bass.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "Like A River SATB Piano.mp3"
+      }
+    ]
+  }
+}
