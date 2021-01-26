d9b2cdf732ff239c9ec838f362f732dff55f54f1
dallinstephens <dallins@juno.com>
2021-01-26 14:24:48 -0700

    Update emitter.js

--- js/emitter.js
+++ js/emitter.js
@@ -280,7 +280,9 @@ $container.on("change", ".link-endpoints", function(e){
   ee.emit("linkendpoints", $(e.target).is(':checked'));
 });
 
-ee.emit("automaticscroll", "true");
+$container.on("load", ".automatic-scroll", function(e){
+  ee.emit("automaticscroll", $(e.target).not(':checked'));
+});
 
 $container.on("change", ".automatic-scroll", function(e){
   ee.emit("automaticscroll", $(e.target).is(':checked'));
