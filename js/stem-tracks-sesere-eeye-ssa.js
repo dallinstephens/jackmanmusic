6a32fc6d4a6343fdcdef98d8d1bbddf7f9872574
dallinstephens <dallins@juno.com>
2021-01-27 08:39:31 -0700

    Update stem-tracks-sesere-eeye-ssa.js

--- js/stem-tracks-sesere-eeye-ssa.js
+++ js/stem-tracks-sesere-eeye-ssa.js
@@ -2,6 +2,7 @@ var playlist = WaveformPlaylist.init({
   samplesPerPixel: 1000,
   waveHeight: 100,
   container: document.getElementById("playlist"),
+  isAutomaticScroll: true,
   timescale: true,
   state: 'cursor',
   colors: {
@@ -13,13 +14,6 @@ var playlist = WaveformPlaylist.init({
   },
   zoomLevels: [500, 1000, 3000, 5000, 500]
 });
-
-var playlist = WaveformPlaylist(
-  {
-    container: document.getElementById("playlist"),
-    isAutomaticScroll: true
-  }
-);
 
 playlist.load([
   {
