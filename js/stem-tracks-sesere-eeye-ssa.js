029e2f36bf75ea810e7bd71611732241f250efed
dallinstephens <dallins@juno.com>
2021-01-27 09:18:41 -0700

    Update stem-tracks-sesere-eeye-ssa.js

--- js/stem-tracks-sesere-eeye-ssa.js
+++ js/stem-tracks-sesere-eeye-ssa.js
@@ -11,7 +11,7 @@ var playlist = WaveformPlaylist.init({
     show: true, //whether or not to include the track controls
     width: 200 //width of controls in pixels
   },
-  zoomLevels: [500, 1000, 3000, 5000, 500]
+  zoomLevels: [500, 1000, 3000, 5000]
 });
 
 playlist.load([
