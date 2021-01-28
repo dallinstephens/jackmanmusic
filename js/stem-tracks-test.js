79e88c4f30b3836ba4a071f436355ab3dcd1b6d2
dallinstephens <dallins@juno.com>
2021-01-28 14:06:52 -0700

    Update stem-tracks-test.js

--- js/stem-tracks-test.js
+++ js/stem-tracks-test.js
@@ -16,19 +16,19 @@ var playlist = WaveformPlaylist.init({
 
 playlist.load([
   {
-    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Melody.mp3",
+    "src": "https://firebasestorage.googleapis.com/v0/b/a-ccompany.appspot.com/o/accompany_online_access%2FSesereEeye_MO_CRT_SSA%2FSesere%20Eeye%20Melody.mp3?alt=media&token=61e4dbf3-0ed2-43a2-96a7-6d3d3c90ee96",
     "name": "Melody",
     "gain": 0.75,
     "muted": false,
     "soloed": false
   },
   {
-    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Harmony%201.mp3",
+    "src": "https://firebasestorage.googleapis.com/v0/b/a-ccompany.appspot.com/o/accompany_online_access%2FSesereEeye_MO_CRT_SSA%2FSesere%20Eeye%20Harmony%201.mp3?alt=media&token=b27da123-e58b-4ad4-a330-457491e1245b",
     "name": "Harmony 1",
     "gain": 0.75
   },
   {
-    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Harmony%202.mp3",
+    "src": "https://firebasestorage.googleapis.com/v0/b/a-ccompany.appspot.com/o/accompany_online_access%2FSesereEeye_MO_CRT_SSA%2FSesere%20Eeye%20Harmony%202.mp3?alt=media&token=221f777f-8707-453c-a089-be39689a1306",
     "name": "Harmony 2",
     "gain": 0.75
   }
