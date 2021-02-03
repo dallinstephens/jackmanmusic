29b09d4fbdc3b5b28c4511ec4b335aafae8ab4ad
dallinstephens <dallins@juno.com>
2021-02-03 16:51:56 -0700

    Add files via upload

---
+++ All of Me/Saxophone/meta.js
@@ -0,0 +1,39 @@
+{
+  "song": {
+    "title": "All of Me",
+    "description": "By Roger Holmes",
+    "composer": "Sax",
+    "albumArt": "All of Me.jpg",
+    "midiFile": "All of Me - Roger Holmes.mid",
+    "track": [
+      {
+        "title": "Alto 1",
+        "filename": "Alto 1_05-06.mp3"
+      },
+      {
+        "title": "Alto 2",
+        "filename": "Alto 2_02-06.mp3"
+      },
+      {
+        "title": "Tenor 1",
+        "filename": "Tenor 1_03-06.mp3"
+      },
+      {
+        "title": "Tenor 2",
+        "filename": "Tenor 2_02-06.mp3"
+      },
+      {
+        "title": "Bari",
+        "filename": "Bari_02-06.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-06.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Sax_01-06.mp3"
+      }
+    ]
+  }
+}
