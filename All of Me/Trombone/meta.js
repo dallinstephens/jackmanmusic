c379083d4e0ec4fad5c013d68d38bcd6658f79d9
dallinstephens <dallins@juno.com>
2021-02-03 16:52:00 -0700

    Add files via upload

---
+++ All of Me/Trombone/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "All of Me",
+    "description": "By Roger Holmes",
+    "composer": "Trombone",
+    "albumArt": "All of Me.jpg",
+    "midiFile": "All of Me - Roger Holmes.mid",
+    "track": [
+      {
+        "title": "Trombone 1",
+        "filename": "Trombone 1_04-06.mp3"
+      },
+      {
+        "title": "Trombone 2",
+        "filename": "Trombone 2_02-06.mp3"
+      },
+      {
+        "title": "Trombone 3",
+        "filename": "Trombone 3_02-06.mp3"
+      },
+      {
+        "title": "Trombone 4",
+        "filename": "Trombone 4_02-06.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-06.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Bones_01-06.mp3"
+      }
+    ]
+  }
+}
