4b9b12fd12f4b0b78d441a0f6916bc3f427bbebc
dallinstephens <dallins@juno.com>
2021-02-03 16:53:13 -0700

    Add files via upload

---
+++ All of Me/Trumpet/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "All of Me",
+    "description": "By Roger Holmes",
+    "composer": "Trumpet",
+    "albumArt": "All of Me.jpg",
+    "midiFile": "All of Me - Roger Holmes.mid",
+    "track": [
+      {
+        "title": "Trumpet 1",
+        "filename": "Trumpet 1_03-06.mp3"
+      },
+      {
+        "title": "Trumpet 2",
+        "filename": "Trumpet 2_02-06.mp3"
+      },
+      {
+        "title": "Trumpet 3",
+        "filename": "Trumpet 3_02-06.mp3"
+      },
+      {
+        "title": "Trumpet 4",
+        "filename": "Trumpet 4_03-06.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-06.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Trumpets_01-06.mp3"
+      }
+    ]
+  }
+}
