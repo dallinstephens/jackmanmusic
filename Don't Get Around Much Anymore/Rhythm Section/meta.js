3323a867f72512c0c075899173dcdd6c4fc25304
dallinstephens <dallins@juno.com>
2021-02-03 16:57:33 -0700

    Add files via upload

---
+++ Don't Get Around Much Anymore/Rhythm Section/meta.js
@@ -0,0 +1,39 @@
+{
+  "song": {
+    "title": "Don't Get Around Much Anymore",
+    "description": "Arr. Mark Taylor",
+    "composer": "Rhythm Section",
+    "albumArt": "Jazz Don't Get Around Much Anymore - Taylor Image.jpg",
+    "midiFile": "14 Don't Get Around Much Anymore - Taylor.mid",
+    "track": [
+      {
+        "title": "Guitar",
+        "filename": "GUIT.01_20.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "KEYS.01_20.mp3"
+      },
+      {
+        "title": "Bass",
+        "filename": "BASS.01_20.mp3"
+      },
+      {
+        "title": "Drums",
+        "filename": "KIT.01_20.mp3"
+      },
+      {
+        "title": "Vibes",
+        "filename": "PERC.1.01_03.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.1.01_02.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "S_B_T.01_20.mp3"
+      }
+    ]
+  }
+}
