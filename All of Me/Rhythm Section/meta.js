2b3a7d7da79662f9c246e9c3462aecb2ff1094d3
dallinstephens <dallins@juno.com>
2021-02-03 16:45:41 -0700

    Add files via upload

---
+++ All of Me/Rhythm Section/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "All of Me",
+    "description": "By Roger Holmes",
+    "composer": "Rhythm Section",
+    "albumArt": "All of Me.jpg",
+    "midiFile": "All of Me - Roger Holmes.mid",
+    "track": [
+      {
+        "title": "Guitar",
+        "filename": "Guitar_04-06.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "Piano _01-06.mp3"
+      },
+      {
+        "title": "Bass",
+        "filename": "Bass _04-06.mp3"
+      },
+      {
+        "title": "Drums",
+        "filename": "Drums_03-06.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-06.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No_01-06.mp3"
+      }
+    ]
+  }
+}
