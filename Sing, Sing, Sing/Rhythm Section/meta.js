2e002b2409a3cb374b4bec78b7c94110f8b5b889
dallinstephens <dallins@juno.com>
2021-02-03 17:07:34 -0700

    Add files via upload

---
+++ Sing, Sing, Sing/Rhythm Section/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "Sing, Sing, Sing",
+    "description": "Arr. Mike Lewis",
+    "composer": "Rhythm Section",
+    "albumArt": "Jazz 08 Sing, Sing, Sing - Lewis Image.jpg",
+    "midiFile": "08 Sing, Sing, Sing - Lewis.mid",
+    "track": [
+      {
+        "title": "Guitar",
+        "filename": "GUIT.01_09.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "KEYS.01_09.mp3"
+      },
+      {
+        "title": "Bass",
+        "filename": "BASS.01_09.mp3"
+      },
+      {
+        "title": "Drums",
+        "filename": "KIT.01_09.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.01_09.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "S_B_T.01_09.mp3"
+      }
+    ]
+  }
+}
