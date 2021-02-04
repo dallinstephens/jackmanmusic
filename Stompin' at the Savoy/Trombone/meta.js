4b72bd46b1b595ccdd5190c4f6616cb724954676
dallinstephens <dallins@juno.com>
2021-02-03 17:17:19 -0700

    Add files via upload

---
+++ Stompin' at the Savoy/Trombone/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "Stompin' At The Savoy",
+    "description": "By Michael Sweeney",
+    "composer": "Trombone",
+    "albumArt": "Stompin'  At The  Savoy.jpg",
+    "midiFile": "Stompin' At The Savoy - Sweeney.mid",
+    "track": [
+      {
+        "title": "Trombone 1",
+        "filename": "Trombone 1_04-78.mp3"
+      },
+      {
+        "title": "Trombone 2",
+        "filename": "Trombone 2_02-78.mp3"
+      },
+      {
+        "title": "Trombone 3",
+        "filename": "Trombone 3_02-78.mp3"
+      },
+      {
+        "title": "Trombone 4",
+        "filename": "Trombone 4_02-78.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-78.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Bones_02-07.mp3"
+      }
+    ]
+  }
+}
