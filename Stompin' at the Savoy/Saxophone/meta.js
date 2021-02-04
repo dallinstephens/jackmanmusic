1459febc97633d68267cbdac75fad522c1afae5e
dallinstephens <dallins@juno.com>
2021-02-03 17:14:48 -0700

    Add files via upload

---
+++ Stompin' at the Savoy/Saxophone/meta.js
@@ -0,0 +1,39 @@
+{
+  "song": {
+    "title": "Stompin' At The Savoy",
+    "description": "By Michael Sweeney",
+    "composer": "Sax",
+    "albumArt": "Stompin'  At The  Savoy.jpg",
+    "midiFile": "Stompin' At The Savoy - Sweeney.mid",
+    "track": [
+      {
+        "title": "Alto 1",
+        "filename": "Alto 1_05-78.mp3"
+      },
+      {
+        "title": "Alto 2",
+        "filename": "Alto 2_02-78.mp3"
+      },
+      {
+        "title": "Tenor 1",
+        "filename": "Tenor 1_04-07.mp3"
+      },
+      {
+        "title": "Tenor 2",
+        "filename": "Tenor 2_02-78.mp3"
+      },
+      {
+        "title": "Bari",
+        "filename": "Bari_02-78.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-78.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Sax_01-78.mp3"
+      }
+    ]
+  }
+}
