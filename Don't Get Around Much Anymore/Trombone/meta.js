ef46fbf34adb8fdac54df8b456e54bfdf62ed2e4
dallinstephens <dallins@juno.com>
2021-02-03 17:01:51 -0700

    Add files via upload

---
+++ Don't Get Around Much Anymore/Trombone/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "Don't Get Around Much Anymore",
+    "description": "Arr. Mark Taylor",
+    "composer": "Trombone",
+    "albumArt": "Jazz Don't Get Around Much Anymore - Taylor Image.jpg",
+    "midiFile": "14 Don't Get Around Much Anymore - Taylor.mid",
+    "track": [
+      {
+        "title": "Trombone 1",
+        "filename": "BONE 1.01_20.mp3"
+      },
+      {
+        "title": "Trombone 2",
+        "filename": "BONE 2.01_20.mp3"
+      },
+      {
+        "title": "Trombone 3",
+        "filename": "BONE 3.01_20.mp3"
+      },
+      {
+        "title": "Trombone 4",
+        "filename": "BONE 4.01_20.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.1.01_02.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "RS_S_T.01_20.mp3"
+      }
+    ]
+  }
+}
