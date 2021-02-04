b766f8807cef59603c8f66c7a8a3bc496ede98e0
dallinstephens <dallins@juno.com>
2021-02-03 17:01:55 -0700

    Add files via upload

---
+++ Don't Get Around Much Anymore/Trumpet/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "Don't Get Around Much Anymore",
+    "description": "Arr. Mark Taylor",
+    "composer": "Trumpet",
+    "albumArt": "Jazz Don't Get Around Much Anymore - Taylor Image.jpg",
+    "midiFile": "14 Don't Get Around Much Anymore - Taylor.mid",
+    "track": [
+      {
+        "title": "Trumpet 1",
+        "filename": "TPT 1.01_20.mp3"
+      },
+      {
+        "title": "Trumpet 2",
+        "filename": "TPT 2.01_20.mp3"
+      },
+      {
+        "title": "Trumpet 3",
+        "filename": "TPT 3.01_20.mp3"
+      },
+      {
+        "title": "Trumpet 4",
+        "filename": "TPT 4.01_20.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.1.01_02.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "RS_S_B.01_20.mp3"
+      }
+    ]
+  }
+}
