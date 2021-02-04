0b33356620e056b33f7412730834064c7cb29bf8
dallinstephens <dallins@juno.com>
2021-02-03 17:10:42 -0700

    Add files via upload

---
+++ Sing, Sing, Sing/Trumpet/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "Sing, Sing, Sing",
+    "description": "Arr. Mike Lewis",
+    "composer": "Trumpet",
+    "albumArt": "Jazz 08 Sing, Sing, Sing - Lewis Image.jpg",
+    "midiFile": "08 Sing, Sing, Sing - Lewis.mid",
+    "track": [
+      {
+        "title": "Trumpet 1",
+        "filename": "TPT 1.01_09.mp3"
+      },
+      {
+        "title": "Trumpet 2",
+        "filename": "TPT 2.01_09.mp3"
+      },
+      {
+        "title": "Trumpet 3",
+        "filename": "TPT 3.01_09.mp3"
+      },
+      {
+        "title": "Trumpet 4",
+        "filename": "TPT 4.01_09.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.01_09.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "RS_S_B.01_09.mp3"
+      }
+    ]
+  }
+}
