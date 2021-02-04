33b603e27bb28bf60d9f300588b278cdb48c327a
dallinstephens <dallins@juno.com>
2021-02-04 16:52:56 -0700

    Update stem-tracks-sing-sing-sing-trombone.js

--- js/stem-tracks-sing-sing-sing-trombone.js
+++ js/stem-tracks-sing-sing-sing-trombone.js
@@ -36,3 +36,28 @@ playlist.load([
 ]).then(function() {
   //can do stuff with the playlist.
 });
+
+      {
+        "title": "Trombone 1",
+        "filename": "BONE 1.01_09.mp3"
+      },
+      {
+        "title": "Trombone 2",
+        "filename": "BONE 2.01_09.mp3"
+      },
+      {
+        "title": "Trombone 3",
+        "filename": "BONE 3.01_09.mp3"
+      },
+      {
+        "title": "Trombone 4",
+        "filename": "BONE 4.01_09.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.01_09.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "RS_S_T.01_09.mp3"
+      }
