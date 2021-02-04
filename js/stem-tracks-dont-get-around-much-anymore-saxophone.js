1b0230d91929a1d5fad9ee0d679b318b3090354d
dallinstephens <dallins@juno.com>
2021-02-04 16:39:38 -0700

    Update stem-tracks-dont-get-around-much-anymore-saxophone.js

--- js/stem-tracks-dont-get-around-much-anymore-saxophone.js
+++ js/stem-tracks-dont-get-around-much-anymore-saxophone.js
@@ -36,3 +36,32 @@ playlist.load([
 ]).then(function() {
   //can do stuff with the playlist.
 });
+
+      {
+        "title": "Alto 1",
+        "filename": "Alto 1.01_20.mp3"
+      },
+      {
+        "title": "Alto 2",
+        "filename": "Alto 2.01_20.mp3"
+      },
+      {
+        "title": "Tenor 1",
+        "filename": "Tenor 1.01_20.mp3"
+      },
+      {
+        "title": "Tenor 2",
+        "filename": "Tenor 2.01_20.mp3"
+      },
+      {
+        "title": "Bari",
+        "filename": "Baritone.01_20.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.1.01_02.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "RS_B_T.01_20.mp3"
+      }
