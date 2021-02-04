7f748b7f7f0f3d86c4def916d0c319c95eca1403
dallinstephens <dallins@juno.com>
2021-02-03 17:10:29 -0700

    Add files via upload

---
+++ Sing, Sing, Sing/Saxophone/meta.js
@@ -0,0 +1,39 @@
+{
+  "song": {
+    "title": "Sing, Sing, Sing",
+    "description": "Arr. Mike Lewis",
+    "composer": "Sax",
+    "albumArt": "Jazz 08 Sing, Sing, Sing - Lewis Image.jpg",
+    "midiFile": "08 Sing, Sing, Sing - Lewis.mid",
+    "track": [
+      {
+        "title": "Alto 1",
+        "filename": "Alto 1.01_09.mp3"
+      },
+      {
+        "title": "Alto 2",
+        "filename": "Alto 2.01_09.mp3"
+      },
+      {
+        "title": "Tenor 1",
+        "filename": "Tenor 1.01_09.mp3"
+      },
+      {
+        "title": "Tenor 2",
+        "filename": "Tenor 2.01_09.mp3"
+      },
+      {
+        "title": "Bari",
+        "filename": "Baritone.01_09.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.01_09.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "RS_B_T.01_09.mp3"
+      }
+    ]
+  }
+}
