b07bc647ee3c1cfe0946f29aa601d9e2ba39c609
dallinstephens <dallins@juno.com>
2021-02-03 17:01:45 -0700

    Add files via upload

---
+++ Don't Get Around Much Anymore/Saxophone/meta.js
@@ -0,0 +1,39 @@
+{
+  "song": {
+    "title": "Don't Get Around Much Anymore",
+    "description": "Arr. Mark Taylor",
+    "composer": "Sax",
+    "albumArt": "Jazz Don't Get Around Much Anymore - Taylor Image.jpg",
+    "midiFile": "14 Don't Get Around Much Anymore - Taylor.mid",
+    "track": [
+      {
+        "title": "Alto 1",
+        "filename": "Alto 1.01_20.mp3"
+      },
+      {
+        "title": "Alto 2",
+        "filename": "Alto 2.01_20.mp3"
+      },
+      {
+        "title": "Tenor 1",
+        "filename": "Tenor 1.01_20.mp3"
+      },
+      {
+        "title": "Tenor 2",
+        "filename": "Tenor 2.01_20.mp3"
+      },
+      {
+        "title": "Bari",
+        "filename": "Baritone.01_20.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.1.01_02.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "RS_B_T.01_20.mp3"
+      }
+    ]
+  }
+}
