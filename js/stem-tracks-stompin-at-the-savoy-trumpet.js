23cf80b293f1eb2d86225f57547bade47893dc48
dallinstephens <dallins@juno.com>
2021-02-04 16:56:31 -0700

    Update stem-tracks-stompin-at-the-savoy-trumpet.js

--- js/stem-tracks-stompin-at-the-savoy-trumpet.js
+++ js/stem-tracks-stompin-at-the-savoy-trumpet.js
@@ -36,3 +36,28 @@ playlist.load([
 ]).then(function() {
   //can do stuff with the playlist.
 });
+
+      {
+        "title": "Trumpet 1",
+        "filename": "Trumpet 1_04-33.mp3"
+      },
+      {
+        "title": "Trumpet 2",
+        "filename": "Trumpet 2_02-78.mp3"
+      },
+      {
+        "title": "Trumpet 3",
+        "filename": "Trumpet 3_03-62.mp3"
+      },
+      {
+        "title": "Trumpet 4",
+        "filename": "Trumpet 4_03-78.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-78.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Trumpets_01-78.mp3"
+      }
