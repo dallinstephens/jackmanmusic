36cac97600911d25bc92a647979b8031f16ad0e4
dallinstephens <dallins@juno.com>
2021-02-04 16:37:20 -0700

    Update stem-tracks-all-of-me-trombone.js

--- js/stem-tracks-all-of-me-trombone.js
+++ js/stem-tracks-all-of-me-trombone.js
@@ -36,3 +36,28 @@ playlist.load([
 ]).then(function() {
   //can do stuff with the playlist.
 });
+
+      {
+        "title": "Trombone 1",
+        "filename": "Trombone 1_04-06.mp3"
+      },
+      {
+        "title": "Trombone 2",
+        "filename": "Trombone 2_02-06.mp3"
+      },
+      {
+        "title": "Trombone 3",
+        "filename": "Trombone 3_02-06.mp3"
+      },
+      {
+        "title": "Trombone 4",
+        "filename": "Trombone 4_02-06.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-06.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Bones_01-06.mp3"
+      }
