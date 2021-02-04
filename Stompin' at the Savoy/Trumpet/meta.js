930b2668f95df3b9452f230e3d51a6714e1bb205
dallinstephens <dallins@juno.com>
2021-02-03 17:16:22 -0700

    Add files via upload

---
+++ Stompin' at the Savoy/Trumpet/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "Stompin' At The Savoy",
+    "description": "By Michael Sweeney",
+    "composer": "Trumpet",
+    "albumArt": "Stompin'  At The  Savoy.jpg",
+    "midiFile": "Stompin' At The Savoy - Sweeney.mid",
+    "track": [
+      {
+        "title": "Trumpet 1",
+        "filename": "Trumpet 1_04-33.mp3"
+      },
+      {
+        "title": "Trumpet 2",
+        "filename": "Trumpet 2_02-78.mp3"
+      },
+      {
+        "title": "Trumpet 3",
+        "filename": "Trumpet 3_03-62.mp3"
+      },
+      {
+        "title": "Trumpet 4",
+        "filename": "Trumpet 4_03-78.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-78.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Trumpets_01-78.mp3"
+      }
+    ]
+  }
+}
