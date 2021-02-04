ad953679c3671a80563d5cf290436867328418c7
dallinstephens <dallins@juno.com>
2021-02-03 17:10:31 -0700

    Add files via upload

---
+++ Sing, Sing, Sing/Trombone/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "Sing, Sing, Sing",
+    "description": "Arr. Mike Lewis",
+    "composer": "Trombone",
+    "albumArt": "Jazz 08 Sing, Sing, Sing - Lewis Image.jpg",
+    "midiFile": "08 Sing, Sing, Sing - Lewis.mid",
+    "track": [
+      {
+        "title": "Trombone 1",
+        "filename": "BONE 1.01_09.mp3"
+      },
+      {
+        "title": "Trombone 2",
+        "filename": "BONE 2.01_09.mp3"
+      },
+      {
+        "title": "Trombone 3",
+        "filename": "BONE 3.01_09.mp3"
+      },
+      {
+        "title": "Trombone 4",
+        "filename": "BONE 4.01_09.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.01_09.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "RS_S_T.01_09.mp3"
+      }
+    ]
+  }
+}
