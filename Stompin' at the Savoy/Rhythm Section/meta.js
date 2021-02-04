bdf383c3056b3090f32ea38e8ec38f894c6deca4
dallinstephens <dallins@juno.com>
2021-02-03 17:14:15 -0700

    Add files via upload

---
+++ Stompin' at the Savoy/Rhythm Section/meta.js
@@ -0,0 +1,35 @@
+{
+  "song": {
+    "title": "Stompin' At The Savoy",
+    "description": "By Michael Sweeney",
+    "composer": "Rhythm Section",
+    "albumArt": "Stompin'  At The  Savoy.jpg",
+    "midiFile": "Stompin' At The Savoy - Sweeney.mid",
+    "track": [
+      {
+        "title": "Guitar",
+        "filename": "Guitar_05-41.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "Piano _09-47.mp3"
+      },
+      {
+        "title": "Bass",
+        "filename": "Bass _04-78.mp3"
+      },
+      {
+        "title": "Drums",
+        "filename": "Drums_03-78.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-78.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No_01-78.mp3"
+      }
+    ]
+  }
+}
