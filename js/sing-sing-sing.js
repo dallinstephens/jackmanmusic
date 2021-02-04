de33b0fb8f1f08ac70b45c242b5b01f9d2ced559
dallinstephens <dallins@juno.com>
2021-02-04 09:59:00 -0700

    Create sing-sing-sing.js

---
+++ js/sing-sing-sing.js
@@ -0,0 +1,38 @@
+var playlist = WaveformPlaylist.init({
+  samplesPerPixel: 1000,
+  waveHeight: 100,
+  container: document.getElementById("playlist"),
+  isAutomaticScroll: true,
+  timescale: true,
+  state: 'cursor',
+  colors: {
+    waveOutlineColor: '#E0EFF1'
+  },
+  controls: {
+    show: true, //whether or not to include the track controls
+    width: 200 //width of controls in pixels
+  },
+  zoomLevels: [500, 1000, 3000, 5000]
+});
+
+playlist.load([
+  {
+    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Melody.mp3",
+    "name": "Melody",
+    "gain": 0.75,
+    "muted": false,
+    "soloed": false
+  },
+  {
+    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Harmony%201.mp3",
+    "name": "Harmony 1",
+    "gain": 0.75
+  },
+  {
+    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Harmony%202.mp3",
+    "name": "Harmony 2",
+    "gain": 0.75
+  }
+]).then(function() {
+  //can do stuff with the playlist.
+});
