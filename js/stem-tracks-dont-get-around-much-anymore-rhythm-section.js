079ec5a03410dbbcba0cbf4003e30dca2dd6c60c
dallinstephens <dallins@juno.com>
2021-02-04 16:38:45 -0700

    Update stem-tracks-dont-get-around-much-anymore-rhythm-section.js

--- js/stem-tracks-dont-get-around-much-anymore-rhythm-section.js
+++ js/stem-tracks-dont-get-around-much-anymore-rhythm-section.js
@@ -36,3 +36,32 @@ playlist.load([
 ]).then(function() {
   //can do stuff with the playlist.
 });
+
+      {
+        "title": "Guitar",
+        "filename": "GUIT.01_20.mp3"
+      },
+      {
+        "title": "Piano",
+        "filename": "KEYS.01_20.mp3"
+      },
+      {
+        "title": "Bass",
+        "filename": "BASS.01_20.mp3"
+      },
+      {
+        "title": "Drums",
+        "filename": "KIT.01_20.mp3"
+      },
+      {
+        "title": "Vibes",
+        "filename": "PERC.1.01_03.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "SOLO.1.01_02.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "S_B_T.01_20.mp3"
+      }
