3e706d5bc0fc5abd052ae0d4f5186c1ca568f7be
dallinstephens <dallins@juno.com>
2021-02-04 16:36:35 -0700

    Update stem-tracks-all-of-me-saxophone.js

--- js/stem-tracks-all-of-me-saxophone.js
+++ js/stem-tracks-all-of-me-saxophone.js
@@ -36,3 +36,32 @@ playlist.load([
 ]).then(function() {
   //can do stuff with the playlist.
 });
+
+      {
+        "title": "Alto 1",
+        "filename": "Alto 1_05-06.mp3"
+      },
+      {
+        "title": "Alto 2",
+        "filename": "Alto 2_02-06.mp3"
+      },
+      {
+        "title": "Tenor 1",
+        "filename": "Tenor 1_03-06.mp3"
+      },
+      {
+        "title": "Tenor 2",
+        "filename": "Tenor 2_02-06.mp3"
+      },
+      {
+        "title": "Bari",
+        "filename": "Bari_02-06.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-06.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Sax_01-06.mp3"
+      }
