f797b4012f0e81765876442c7d356b9306466063
dallinstephens <dallins@juno.com>
2021-02-04 16:37:56 -0700

    Update stem-tracks-all-of-me-trumpet.js

--- js/stem-tracks-all-of-me-trumpet.js
+++ js/stem-tracks-all-of-me-trumpet.js
@@ -36,3 +36,28 @@ playlist.load([
 ]).then(function() {
   //can do stuff with the playlist.
 });
+
+      {
+        "title": "Trumpet 1",
+        "filename": "Trumpet 1_03-06.mp3"
+      },
+      {
+        "title": "Trumpet 2",
+        "filename": "Trumpet 2_02-06.mp3"
+      },
+      {
+        "title": "Trumpet 3",
+        "filename": "Trumpet 3_02-06.mp3"
+      },
+      {
+        "title": "Trumpet 4",
+        "filename": "Trumpet 4_03-06.mp3"
+      },
+      {
+        "title": "Solo",
+        "filename": "Solo_02-06.mp3"
+      },
+      {
+        "title": "Band",
+        "filename": "Band No Trumpets_01-06.mp3"
+      }
