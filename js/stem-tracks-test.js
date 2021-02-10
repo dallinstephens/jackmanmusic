c2aba18e890c30259818eaad05fbaf8b9c04d0a8
dallinstephens <dallins@juno.com>
2021-02-10 11:02:10 -0700

    Update stem-tracks-test.js

--- js/stem-tracks-test.js
+++ js/stem-tracks-test.js
@@ -207,10 +207,7 @@ var actions = [
 ];
 
 var playlist = WaveformPlaylist.init({
-  samplesPerPixel: 1000,
-  waveHeight: 100,
   container: document.getElementById("playlist"),
-  isAutomaticScroll: true,
   timescale: true,
   state: 'select',
   samplesPerPixel: 1024,
@@ -225,33 +222,13 @@ var playlist = WaveformPlaylist.init({
     editable: true,
     isContinuousPlay: false,
     linkEndpoints: true
-  },
-  controls: {
-    show: true, //whether or not to include the track controls
-    width: 200 //width of controls in pixels
-  },
-  zoomLevels: [500, 1000, 3000, 5000]
+  }
 });
 
 playlist.load([
   {
-    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Melody.mp3",
-    "name": "Melody",
-    "gain": 0.75,
-    "muted": false,
-    "soloed": false
-  },
-  {
-    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Harmony%201.mp3",
-    "name": "Harmony 1",
-    "gain": 0.75
-  },
-  {
-    "src": "SesereEeye_MO_CRT_SSA/Sesere%20Eeye%20Harmony%202.mp3",
-    "name": "Harmony 2",
-    "gain": 0.75
+    src: "media/audio/sonnet.mp3"
   }
 ]).then(function() {
   //can do stuff with the playlist.
-  document.getElementById("load-box").style.display="none"; // closes loading box with progress bar
 });
